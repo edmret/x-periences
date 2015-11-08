9261c76072261ac6be25a6e5a58df06f7d979dc3
hyliant <racso_181088@hotmail.com>
2015-11-08 03:44:54 -0600

    Initial feeling catalog

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,4 +1,5 @@
 Meteor.startup(function() {
+  //Categories collection initialization
   if(typeof Categories.findOne() === 'undefined') {
     Categories.insert({name: "Finanzas"});
     Categories.insert({name: "Deportes"});
@@ -9,5 +10,17 @@ Meteor.startup(function() {
     Categories.insert({name: "Educación"});
     Categories.insert({name: "Familia"});
     Categories.insert({name: "Hogar"});
+  }
+
+  //Feeling collection initialization
+  if(typeof Feelings.findOne() === 'undefined') {
+    Feelings.insert({name_es: "Alegría", name: "Joy", enum: 0});
+    Feelings.insert({name_es: "Anticipación", name: "Anticipation", enum: 1});
+    Feelings.insert({name_es: "Ira", name: "Anger", enum: 2});
+    Feelings.insert({name_es: "Aversión", name: "Disgust", enum: 3});
+    Feelings.insert({name_es: "Tristeza", name: "Sadness", enum: 4});
+    Feelings.insert({name_es: "Sorpresa", name: "Surprise", enum: 5});
+    Feelings.insert({name_es: "Miedo", name: "Fear", enum: 6});
+    Feelings.insert({name_es: "Confianza", name: "Trust", enum: 7}); 
   }
 });
